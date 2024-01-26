e2d8303e711021fd391dc803e858bfef7d7d4bd1
root <kevin.ferati@heig-vd.cch>
2024-01-26 09:24:40 +0000

    merde

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [TasksController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
